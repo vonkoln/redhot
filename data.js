9703e415689257d627ad92bae06c5683cd0f27b1
Bruno <brunovonkoln@gmail.com>
2025-10-28 02:41:13 -0300

    ..

---
+++ data.js
@@ -0,0 +1,63 @@
+export default  [  
+  {
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "1.mp3",
+  },  
+  {
+    title:"2 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "2.mp3",
+  },  
+  {
+    title:"3 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "3.mp3",
+  },  
+  {
+    title:"4 -",
+    artist: "Remix Taos Elieri",
+    cover: "1.jpeg",
+    file: "4.mp3",
+  },  
+  {
+    title:"5 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "5.mp3",
+  },  
+  {
+    title:"6 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "6.mp3",
+  },  
+  {
+    title:"7 - ",
+    artist: "",
+    cover: "1.jpeg",
+    file: "7.mp3",
+  },  
+  {
+    title:"8 -,
+    artist: "Taos Elieri",
+    cover: "1.jpeg",
+    file: "8.mp3",
+  },  
+  {
+    title:"9 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "9.mp3",
+  },  
+  {
+    title:"10 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "10.mp3",
+  }
+ ];
+
